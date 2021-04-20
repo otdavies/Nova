ea8002b9bd17c83b3525e3c05dc3afb73692e1ff
HalenTech <chalen@halentech.net>
2021-04-20 01:15:42 -0700

    defual patten

--- src/arduino/RemoteLEDControl/src/main.cpp
+++ src/arduino/RemoteLEDControl/src/main.cpp
@@ -17,22 +17,25 @@
 #include "FastLED.h"
 
 // LED constants
-#define NUM_DATA_OUT 4
-#define NUM_LEDS_PER_DATA_OUT 120
+#define NUM_DATA_OUT 5
+#define NUM_LEDS_PER_DATA_OUT 150
 #define NUM_LEDS_TOTAL NUM_LEDS_PER_DATA_OUT *NUM_DATA_OUT
 #define NUM_LEDS_MAX_PER_DATA_OUT 480
+#define MHZ 14
 
 // Data pin that led data will be written out over
-#define DATA_PIN_0 2
-#define DATA_PIN_1 3
-#define DATA_PIN_2 4
-#define DATA_PIN_3 5
+#define DATA_PIN_0 0
+#define DATA_PIN_1 1
+#define DATA_PIN_2 2
+#define DATA_PIN_3 3
+#define DATA_PIN_4 18
 
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
-#define CLOCK_PIN_0 6
-#define CLOCK_PIN_1 7
-#define CLOCK_PIN_2 8
-#define CLOCK_PIN_3 9
+#define CLOCK_PIN_0 4
+#define CLOCK_PIN_1 5
+#define CLOCK_PIN_2 6
+#define CLOCK_PIN_3 7
+#define CLOCK_PIN_4 19
 
 CRGB leds[NUM_DATA_OUT][NUM_LEDS_PER_DATA_OUT];
 
@@ -50,10 +53,11 @@ EthernetUDP Udp;
 
 void setupLEDs()
 {
-  FastLED.addLeds<APA102, DATA_PIN_0, CLOCK_PIN_0, RGB, DATA_RATE_MHZ(2)>(leds[0], NUM_LEDS_PER_DATA_OUT);
-  FastLED.addLeds<APA102, DATA_PIN_1, CLOCK_PIN_1, RGB, DATA_RATE_MHZ(2)>(leds[1], NUM_LEDS_PER_DATA_OUT);
-  FastLED.addLeds<APA102, DATA_PIN_2, CLOCK_PIN_2, RGB, DATA_RATE_MHZ(2)>(leds[2], NUM_LEDS_PER_DATA_OUT);
-  FastLED.addLeds<APA102, DATA_PIN_3, CLOCK_PIN_3, RGB, DATA_RATE_MHZ(2)>(leds[3], NUM_LEDS_PER_DATA_OUT);
+  FastLED.addLeds<APA102, DATA_PIN_0, CLOCK_PIN_0, BGR, DATA_RATE_MHZ(MHZ)>(leds[0], NUM_LEDS_PER_DATA_OUT);
+  FastLED.addLeds<APA102, DATA_PIN_1, CLOCK_PIN_1, BGR, DATA_RATE_MHZ(MHZ)>(leds[1], NUM_LEDS_PER_DATA_OUT);
+  FastLED.addLeds<APA102, DATA_PIN_2, CLOCK_PIN_2, BGR, DATA_RATE_MHZ(MHZ)>(leds[2], NUM_LEDS_PER_DATA_OUT);
+  FastLED.addLeds<APA102, DATA_PIN_3, CLOCK_PIN_3, BGR, DATA_RATE_MHZ(MHZ)>(leds[3], NUM_LEDS_PER_DATA_OUT);
+  FastLED.addLeds<APA102, DATA_PIN_4, CLOCK_PIN_4, BGR, DATA_RATE_MHZ(MHZ)>(leds[4], NUM_LEDS_PER_DATA_OUT);
 }
 
 void setupUDP()
@@ -90,7 +94,7 @@ void setup()
 {
   setupUDP();
   delay(100);
-  // setupLEDs();
+  setupLEDs();
 }
 
 void prettyPrint(int packetSize)
@@ -113,7 +117,41 @@ void prettyPrint(int packetSize)
 }
 
 int count = 0;
+
+void DefaultPattern()
+{
+   // Move a single white led 
+   for(int idx = 0; idx < NUM_LEDS_PER_DATA_OUT; idx = idx + 1) 
+   {
+      // Turn our current led on to white, then show the leds
+      leds[0][idx] = CRGB::Red;
+      leds[1][idx] = CRGB::Green;
+      leds[2][idx] = CRGB::Blue;
+      leds[3][idx] = CRGB::Yellow; // / 128;
+      leds[4][idx] = CRGB::White;
+      digitalWrite(13, HIGH);
+
+      // Show the leds (only one of which is set to white, from above)
+      FastLED.show();
+
+      // Wait a little bit
+      delay(35);
+
+      // Turn our current led back to black for the next loop around
+      leds[0][idx] = CRGB::Black;
+      leds[1][idx] = CRGB::Black;
+      leds[2][idx] = CRGB::Black;
+      leds[3][idx] = CRGB::Black;
+      leds[4][idx] = CRGB::Black;
+      digitalWrite(13, LOW);
+      
+
+      
+   }
+}
+
 void loop()
+
 {
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
@@ -136,4 +174,9 @@ void loop()
     Udp.write((byte)0);
     Udp.endPacket();
   }
+  else {
+     DefaultPattern();
+  }
+  
 }
+
